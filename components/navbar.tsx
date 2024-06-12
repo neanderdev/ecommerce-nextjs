4ea01c6da08c3f610d1369ee85fa1c98b9acf502
Neander de Souza <neander.faria@hotmail.com>
2024-06-12 15:51:38 -0300

    Navigation bar (Admin)

---
+++ components/navbar.tsx
@@ -0,0 +1,40 @@
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
+import { MainNav } from "./main-nav";
+import { StoreSwitcher } from "./store-switcher";
+
+export async function Navbar() {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/sign-in");
+    }
+
+    const stores = await prismadb.store.findMany({
+        where: {
+            userId,
+        },
+    });
+
+    return (
+        <div className="border-b">
+            <div className="flex items-center h-16 px-4">
+                <StoreSwitcher
+                    items={stores}
+                />
+
+                <MainNav className="mx-6" />
+
+                <div className="ml-auto flex items-center space-x-4">
+                    <UserButton
+                        afterSignOutUrl="/"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
